0b37cf60b3e1df3ae8c45033e32a7c4f3c5ebe5d
Cendyne <cendyne@cendyne.dev>
2021-12-26 02:08:21 -0600

    Add more formats

---
+++ static/bread.sql
@@ -0,0 +1,9 @@
+CREATE TABLE glitch(
+   mouth VARCHAR(8) NOT NULL PRIMARY KEY
+);
+INSERT INTO glitch(mouth) VALUES ('baguette');
+INSERT INTO glitch(mouth) VALUES ('baguette');
+INSERT INTO glitch(mouth) VALUES ('baguette');
+INSERT INTO glitch(mouth) VALUES ('baguette');
+INSERT INTO glitch(mouth) VALUES ('baguette');
+INSERT INTO glitch(mouth) VALUES ('baguette');
